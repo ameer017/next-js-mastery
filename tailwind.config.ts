c908413e0e67a864011f30c9b5eb22e2cd0cf029
ameer017 <rajiabdullahi907@gmail.com>
2023-11-29 14:37:12 +0100

    second commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,12 @@ const config: Config = {
       //   'gradient-conic':
       //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       // },
-      
+      colors: {
+        primary: {
+          500: '#ff7000',
+          100 : '#fff1e6'
+        }
+      }
     },
   },
   plugins: [],
