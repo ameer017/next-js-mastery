3f9558fef1d73902885bfe25350c6ac000d16890
ameer017 <rajiabdullahi907@gmail.com>
2023-11-29 14:38:38 +0100

    3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      //   'gradient-conic':
-      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      // },
       colors: {
         primary: {
           500: '#ff7000',
-          100 : '#fff1e6'
+          100 : '#fff1e6',
+
+        },
+        dark: {
+          100: '#000000',
+          200: '#0f1117'
         }
       }
     },
