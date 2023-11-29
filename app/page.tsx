fd52d23b758dd58c4ae4d11c51fa449a16825121
ameer017 <rajiabdullahi907@gmail.com>
2023-11-29 15:57:30 +0100

    2

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Next.js 13, we&apos;re coming!</h1>
+      <h2 className="h2-bold">Next.js 13, we&apos;re coming!</h2>
     </div>
   );
 }
