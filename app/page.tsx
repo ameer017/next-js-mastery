ae40f1f0715b3f6d988987e089b49c923311102b
ameer017 <rajiabdullahi907@gmail.com>
2023-11-29 16:08:15 +0100

    009_last commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <h2 className="h2-bold">Next.js 13, we&apos;re coming!</h2>
       <h3 className="h3-bold">Next.js 13, we&apos;re coming!</h3>
       <h4 className="h4-bold">Next.js 13, we&apos;re coming!</h4>
+      <h5 className="h5-bold">Next.js 13, we&apos;re coming!</h5>
     </div>
   );
 }
